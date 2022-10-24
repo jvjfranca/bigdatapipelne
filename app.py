743731f774003f68614d038185896c8faafecaf3
Vicente <jvjfranca@gmail.com>
2022-10-24 17:09:01 +0000

    lala

--- app.py
+++ app.py
@@ -1,9 +1,39 @@
-#!/usr/bin/env python3
 
 import aws_cdk as cdk
+from aws_ddk_core.cicd import CICDPipelineStack
 from ddk_app.ddk_app_stack import DdkApplicationStack
+from aws_ddk_core.config import Config
+
 
 app = cdk.App()
-DdkApplicationStack(app, "DdkApplicationStack", "dev")
+
+
+class ApplicationStage(cdk.Stage):
+    def __init__(
+        self,
+        scope,
+        environment_id: str,
+        **kwargs,
+    ) -> None:
+        super().__init__(
+            scope, f"Ddk{environment_id.title()}Application", **kwargs)
+        DdkApplicationStack(self, "DataPipeline", environment_id)
+
+
+config = Config()
+(
+    CICDPipelineStack(
+        app,
+        id="DdkCodePipeline",
+        environment_id="cicd",
+        pipeline_name="ddk-application-pipeline",
+    )
+    .add_source_action(repository_name="ddk-repository")
+    .add_synth_action()
+    .build()
+    .add_stage("dev", ApplicationStage(app, "dev", env=config.get_env("dev")))
+    .add_stage("hom", ApplicationStage(app, "hom", env=config.get_env("hom")))
+    .synth()
+)
 
 app.synth()
