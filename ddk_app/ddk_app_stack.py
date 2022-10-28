4c5ff5706fea4528c96dae58fa583a115261a2d1
jvjfranca <jvjfranca@gmail.com>
2022-10-28 12:47:52 -0300

    novo firehose jq

--- ddk_app/ddk_app_stack.py
+++ ddk_app/ddk_app_stack.py
@@ -1,21 +1,25 @@
 from typing import Any
-
-from aws_cdk.aws_kinesis import IStream, Stream, StreamEncryption, StreamMode
+from aws_cdk import (
+    aws_kinesisfirehose_alpha as firehose,
+    aws_kinesis,
+    aws_kms as kms,
+    RemovalPolicy,
+    Duration,
+    Size
+)
+from aws_cdk.aws_kinesisfirehose import (
+    CfnDeliveryStream
+)
 from aws_ddk_core.base import BaseStack
+
 from aws_ddk_core.resources import (
-    DataBrewFactory as brew,
-    GlueFactory as glue,
-    KinesisFirehoseFactory as firehose,
+    S3Factory as s3,
     KinesisStreamsFactory as dstream,
     KMSFactory as kms,
-    LambdaFactory as lmb,
-    S3Factory as s3,
-    SQSFactory as sqs
 )
 from aws_ddk_core.stages import (
-    KinesisToS3Stage
+    KinesisToS3Stage as streams3
 )
-from aws_cdk import Duration
 from constructs import Construct
 
 
@@ -32,17 +36,86 @@ class DdkApplicationStack(BaseStack):
             environment_id,
         )
 
+        key = kms.Key(self, "Key", removal_policy=RemovalPolicy.DESTROY)
+
+
         data_stream = dstream.data_stream(
             self,
             "card-stream",
             environment_id,
-            encryption=StreamEncryption.MANAGED,
+            encryption=aws_kinesis.StreamEncryption.MANAGED,
             retention_period=Duration.days(30),
-            stream_mode=StreamMode.ON_DEMAND,
+            stream_mode=aws_kinesis.StreamMode.ON_DEMAND,
             stream_name="card-stream"
         )
 
-        stage_firehose_s3 = KinesisToS3Stage(
+        destination_config = firehose.DestinationConfig(
+            extended_s3_destination_configuration=CfnDeliveryStream.ExtendedS3DestinationConfigurationProperty(
+                bucket_arn=card_data.bucket_arn,
+                role_arn="roleArn",
+                # the properties below are optional
+                buffering_hints=CfnDeliveryStream.BufferingHintsProperty(
+                    interval_in_seconds=300,
+                    size_in_mBs=64
+                ),
+                cloud_watch_logging_options=CfnDeliveryStream.CloudWatchLoggingOptionsProperty(
+                    enabled=False,
+                    log_group_name="logGroupName",
+                    log_stream_name="logStreamName"
+                ),
+                compression_format="GZIP",
+                enabled=True,
+                dynamic_partitioning_configuration=CfnDeliveryStream.DynamicPartitioningConfigurationProperty(
+                    enabled=True,
+                    retry_options=CfnDeliveryStream.RetryOptionsProperty(duration_in_seconds=300)
+                ),
+                encryption_configuration=CfnDeliveryStream.EncryptionConfigurationProperty(
+                    kms_encryption_config=CfnDeliveryStream.KMSEncryptionConfigProperty(
+                        awskms_key_arn=key.key_arn
+                    )
+                ),
+                error_output_prefix="errorOutputPrefix",
+                prefix="data/UF=!{partitionKeyFromQuery:uf}",
+                processing_configuration=CfnDeliveryStream.ProcessingConfigurationProperty(
+                    enabled=True,
+                    processors=[
+                        CfnDeliveryStream.ProcessorProperty(
+                            type="MetadataExtraction",
+                            # the properties below are optional
+                            parameters=[
+                                CfnDeliveryStream.ProcessorParameterProperty(
+                                    parameter_name="MetadataExtractionQuery",
+                                    parameter_value="{uf:.uf}"
+                                ),
+                                CfnDeliveryStream.ProcessorParameterProperty(
+                                    parameter_name="JsonParsingEngine",
+                                    parameter_value="JQ-1.6"
+                                )
+                            ]
+                        )
+                    ]
+                )
+            )
+        )
+
+        delivery_stream = firehose.DeliveryStream(
+            self,
+            'firehose',
+            destinations=destination_config,
+            source_stream=data_stream
+        )
+
+        test_stage = streams3(
+            self,
+            "card-data-ingestion2",
+            environment_id,
+            bucket=card_data,
+            delivery_stream=delivery_stream,
+            data_stream=data_stream
+        )
+
+
+        stage_firehose_s3 = streams3(
             self,
             "card-data-ingestion",
             environment_id,
