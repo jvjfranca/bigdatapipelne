6f4163128528feaad0d7e4909c2408daf6ac2a42
jvjfranca <jvjfranca@gmail.com>
2022-11-05 10:16:11 -0300

    lambda print consumer stream

--- lambda/consumer/function.py
+++ lambda/consumer/function.py
@@ -2,6 +2,7 @@ import base64
 import boto3
 from time import time
 from os import environ
+import json
 
 ddb_client = boto3.client('dynamodb')
 TABLE = environ.get['TABLE']
@@ -11,6 +12,7 @@ def handler(event, context):
     ttl = int(time.time() + 24*3600*30)
     for record in event['Records']:
         payload = base64.b64decode(record['kinesis']['data']).decode('utf-8')
+        print(json.dumps(payload, indent=2))
         ddb_client.put_item(
             TableName=TABLE,
             Item={
